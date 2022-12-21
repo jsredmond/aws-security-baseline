5862fab89e6fb446995a055ecd3cd613295f2f82
jsredmond <norrinscott@gmail.com>
2022-12-21 15:15:37 -0500

    Test linter

--- s3.tf
+++ s3.tf
@@ -12,6 +12,7 @@ resource "aws_s3_bucket_public_access_block" "ct-bucket-acl" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+  aklshdjfhkjasd
 }
 
 # CloudTrail bucket policy

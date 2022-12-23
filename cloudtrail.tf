2ec1ee05bf2a7a11ba8ad3a048aebb67d4532eb3
jsredmond <110986047+jsredmond@users.noreply.github.com>
2022-12-22 21:04:46 -0500

    s3 and cloudtrail changes

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -7,4 +7,6 @@ resource "aws_cloudtrail" "cloudtrail" {
   s3_bucket_name                = aws_s3_bucket.ct-bucket.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = true
+  enable_log_file_validation    = true
+  is_multi_region_trail         = true
 }

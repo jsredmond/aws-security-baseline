45c566b9fa58d648c70a4be1ee245556b93077b8
jsredmond <110986047+jsredmond@users.noreply.github.com>
2022-12-29 19:36:01 -0500

    cloudtrail fixes

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,6 +1,12 @@
 # Used to call the AWS account ID
 data "aws_caller_identity" "current" {}
 
+resource "aws_cloudwatch_log_group" "cloudwatch-ct-logs" {
+  name = "cloudwatch-ct-logs"
+  retention_in_days = 30
+  kms_key_id = aws_kms_key.s3key.arn
+}
+
 # Enabling CloudTrail
 resource "aws_cloudtrail" "cloudtrail" {
   name                          = "${var.prefix_name}-cloudtrail"
@@ -9,4 +15,7 @@ resource "aws_cloudtrail" "cloudtrail" {
   include_global_service_events = true
   enable_log_file_validation    = true
   is_multi_region_trail         = true
+  kms_key_id                    = aws_kms_key.s3key.arn
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudwatch-ct-logs.arn}:*"
+
 }

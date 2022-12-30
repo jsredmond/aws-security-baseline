028169e8b6a37bf3adcf840f78fae32584ea66d9
jsredmond <110986047+jsredmond@users.noreply.github.com>
2022-12-29 19:57:23 -0500

    CloudTrail fixes

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,10 +1,17 @@
 # Used to call the AWS account ID
 data "aws_caller_identity" "current" {}
 
+# KMS key to encrypt CloudWatch log group
+resource "aws_kms_key" "ctkey" {
+  description             = "This key is used to encrypt an eks cloudwatch log group"
+  deletion_window_in_days = 7
+}
+
+# Create CloudWatch log group for CloudTrail
 resource "aws_cloudwatch_log_group" "cloudwatch-ct-logs" {
-  name = "cloudwatch-ct-logs"
+  name              = "cloudwatch-ct-logs"
   retention_in_days = 30
-  kms_key_id = aws_kms_key.s3key.arn
+  kms_key_id        = aws_kms_key.ctkey.arn
 }
 
 # Enabling CloudTrail
